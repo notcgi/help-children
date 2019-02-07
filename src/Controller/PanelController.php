2f265ba2bd979ecfbccc5754a19c5f69c60ab9d2
Botik <botik213@gmail.com>
2019-02-07 16:48:46 +0300

    Example work with forms

--- src/Controller/PanelController.php
+++ src/Controller/PanelController.php
@@ -2,29 +2,66 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use App\Entity\Task;
-//use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class PanelController extends AbstractController
 {
-    public function auth()
+    public function auth(Request $request)
     {
         // creates a task and gives it some dummy data for this example
-        $task = new Task();
+        $user = new User();
 
-        $form = $this->createFormBuilder($task)
-            ->add('email', EmailType::class)
-            ->add('password', PasswordType::class)
-            ->add('save', SubmitType::class, ['label' => 'Submit', 'attr'=>['class'=>'btn btn-primary']])
+        $form = $this->createFormBuilder($user)
+            ->add(
+                'email',
+                EmailType::class,
+                [
+                    'constraints' => [
+                        new NotBlank(),
+                        new Email(),
+                    ],
+                ]
+            )
+            ->add(
+                'pass',
+                PasswordType::class,
+                [
+                    'constraints' => [
+                        new NotBlank(),
+                        new Length(['min' => 6, 'max' => 32]),
+                    ],
+                ]
+            )
+            ->add(
+                'save',
+                SubmitType::class,
+                [
+                    'label' => 'Submit',
+                    'attr' => ['class' => 'btn btn-primary'],
+                ]
+            )
             ->getForm();
 
-        return $this->render('panel/auth.twig', [
-            'form' => $form->createView(),
-        ]);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted()) {
+            var_dump($form->getErrors(true, true)[0]->getMessage());
+        }
+
+        return $this->render(
+            'panel/auth.twig',
+            [
+                'form' => $form->createView(),
+            ]
+        );
     }
 
     public function users()
