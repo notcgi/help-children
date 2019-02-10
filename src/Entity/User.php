aabbedf6424c8cfd1be564cde3432b670be681b8
Botik <botik213@gmail.com>
2019-02-10 23:37:55 +0300

    Add Authorization

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -3,11 +3,13 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ * @ORM\Table(name='users')
  */
-class User
+class User implements UserInterface
 {
     /**
      * @ORM\Id()
@@ -17,12 +19,12 @@ class User
     private $id;
 
     /**
-     * @ORM\Column(type="smallint", options={"default":1})
+     * @ORM\Column(type="json")
      */
-    private $type = 1;
+    private $roles = [];
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="string", length=180, unique=true)
      */
     private $email;
 
@@ -67,14 +69,31 @@ class User
         return $this->id;
     }
 
-    public function getType(): ?int
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUsername(): string
     {
-        return $this->type;
+        return (string) $this->email;
     }
 
-    public function setType(int $type): self
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
     {
-        $this->type = $type;
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
 
         return $this;
     }
@@ -89,6 +108,14 @@ class User
         $this->email = $email;
 
         return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getPassword(): string
+    {
+        return (string) $this->pass;
     }
 
     public function getPass(): ?string
@@ -173,5 +200,22 @@ class User
         $this->referrals = $referrals;
 
         return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getSalt()
+    {
+        // not needed when using the "bcrypt" algorithm in security.yaml
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials()
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
     }
 }
--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -19,10 +19,11 @@ class UserRepository extends ServiceEntityRepository
         parent::__construct($registry, User::class);
     }
 
-    /**
-     * @return User[] Returns an array of User objects
-     */
-    /*public function findByExampleField($value)
+    // /**
+    //  * @return User[] Returns an array of User objects
+    //  */
+    /*
+    public function findByExampleField($value)
     {
         return $this->createQueryBuilder('u')
             ->andWhere('u.exampleField = :val')
@@ -32,7 +33,8 @@ class UserRepository extends ServiceEntityRepository
             ->getQuery()
             ->getResult()
         ;
-    }*/
+    }
+    */
 
     /*
     public function findOneBySomeField($value): ?User
