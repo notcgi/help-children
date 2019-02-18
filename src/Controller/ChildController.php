4797eb16efbfd8d4e7a12114063e49b0ed1712de
Vlad <shikorinvladislav@gmail.com>
2019-02-18 16:58:32 +0300

    Add child show

--- src/Controller/ChildController.php
+++ src/Controller/ChildController.php
@@ -2,13 +2,63 @@
 
 namespace App\Controller;
 
+use App\Entity\Child;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ChildController extends AbstractController
 {
-    public function view()
+    public function view($id)
     {
-        return $this->render('child/view.twig');
+        $child = $this->getDoctrine()
+            ->getRepository(Child::class)
+            ->find($id);
+
+        if (!$child) {
+            throw $this->createNotFoundException(
+                'Нет ребенка с id ' . $id
+            );
+        }
+
+        $leftGoal = $child->getGoal() - $child->getCollected();
+        $statusGoal = $this->statusGoal($child->getCollected(), $child->getGoal());
+        $ageChildren = $this->ageChildren($child->getBirthdate(), date_create(date('Y-m-d')));
+
+        return $this->render(
+            'child/view.twig',
+            [
+                'child' => $child,
+                'ageChildren' => $ageChildren,
+                'leftGoal' => $leftGoal > 0 ? $leftGoal : 0,
+                'statusGoal' => $statusGoal
+            ]
+        );
+    }
+
+    public function ageChildren($birthDate, $todayDate)
+    {
+        if ($birthDate >= $todayDate) {
+            return 0;
+        }
+
+        $ageChildren = date_diff($birthDate, $todayDate);
+
+        return $ageChildren->y;
+    }
+
+    public function statusGoal($collected = 0, $goal = 0)
+    {
+        if ($collected < 0) {
+            return 0;
+        }
+
+        if ($goal <= 0) {
+            return 0;
+        }
+        if ($collected > $goal) {
+            return 100;
+        }
+
+        return 100 * $collected / $goal;
     }
 
     public function registration()
@@ -38,7 +88,10 @@ class ChildController extends AbstractController
 
     public function kids()
     {
-        return $this->render('child/kids.twig');
+        return $this->render(
+            'child/kids.twig',
+            ['kids' => $this->getDoctrine()->getRepository(Child::class)->findAll()]
+        );
     }
 
     public function login()
