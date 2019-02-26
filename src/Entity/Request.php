7d4587d47b96242b4862c5dce72d0251f93a3b8e
Botik <botik213@gmail.com>
2019-02-26 10:13:27 +0300

    Fix entities

---
+++ src/Entity/Request.php
@@ -0,0 +1,145 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\RequestRepository")
+ * @ORM\Table(name="requests")
+ */
+class Request
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer", options={"unsigned":true})
+     */
+    private $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Child", inversedBy="history", fetch="LAZY")
+     * @ORM\JoinColumn(name="child_id", referencedColumnName="id")
+     */
+    private $child;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="User", inversedBy="requests", fetch="LAZY")
+     * @ORM\JoinColumn(name="user_id", referencedColumnName="id")
+     */
+    private $user;
+
+    /**
+     * @ORM\Column(type="decimal", precision=10, scale=2)
+     */
+    private $sum = .0;
+
+    /**
+     * @ORM\Column(type="smallint", options={"unsigned":true})
+     */
+    private $status = 0;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $recurent = false;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $updatedAt;
+
+    /**
+     * @ORM\Column(type="datetime_immutable")
+     */
+    private $createdAt;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getChild(): ?Child
+    {
+        return $this->child;
+    }
+
+    public function setChildID(?Child $child): self
+    {
+        $this->child = $child;
+
+        return $this;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUserID(User $user): self
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    public function getSum(): float
+    {
+        return $this->sum;
+    }
+
+    public function setSum(float $sum): self
+    {
+        $this->sum = $sum;
+
+        return $this;
+    }
+
+    public function getStatus(): ?int
+    {
+        return $this->status;
+    }
+
+    public function setStatus(int $status): self
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    public function isRecurent(): ?bool
+    {
+        return $this->recurent;
+    }
+
+    public function setRecurent(bool $recurent): self
+    {
+        $this->recurent = $recurent;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeInterface
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(?\DateTimeInterface $updatedAt): self
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+}
