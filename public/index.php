d9101b5e3d38c9ec2087ceb23495ec63790d7d53
Avtorkoda <avtor@avtorkoda.com.ua>
2019-07-03 21:33:40 +0300

    add recurrent payment

--- public/index.php
+++ public/index.php
@@ -11,7 +11,6 @@ if ($_SERVER['APP_DEBUG']) {
 
     Debug::enable();
 }
-    Debug::enable();
 
 if ($trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? $_ENV['TRUSTED_PROXIES'] ?? false) {
     Request::setTrustedProxies(explode(',', $trustedProxies), Request::HEADER_X_FORWARDED_ALL ^ Request::HEADER_X_FORWARDED_HOST);
