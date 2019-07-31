d3952dbda875ca96975ea54c1639d5b30f602212
Alexander Anchishkin <alexanderanchishkin@gmail.com>
2019-08-01 02:38:41 +0300

    Добавлена возможно отправки подтверждения почты через личный кабинет. Там же отображается статус почты (подтверждена / не подтверждена).

--- public/js/validate.js
+++ public/js/validate.js
@@ -52,3 +52,34 @@ function validate_reset() {
     
     return isValid;
 }
+
+function send_email_confirm_code() {
+    let btnStatus = document.querySelector('#btnStatus');
+    let btnSend = document.querySelector('#btnSend');
+
+    btnStatus.textContent = 'Письмо отправлено.';
+    btnSend.style.display = 'None';
+
+    var data = "email=alexan999%40yandex.ru";
+
+    var xhr = new XMLHttpRequest();
+    xhr.withCredentials = true;
+
+    xhr.addEventListener("readystatechange", function () {
+    if (this.readyState === 4) {        
+    }
+    });
+
+    xhr.open("POST", "https://test.xn--c1accbmwfjbh2bd3o.xn--p1ai/user/sendConfirm");
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xhr.setRequestHeader("User-Agent", "PostmanRuntime/7.15.2");
+    xhr.setRequestHeader("Accept", "*/*");
+    xhr.setRequestHeader("Cache-Control", "no-cache");    
+    xhr.setRequestHeader("Host", "test.xn--c1accbmwfjbh2bd3o.xn--p1ai");
+    xhr.setRequestHeader("Accept-Encoding", "gzip, deflate");
+    xhr.setRequestHeader("Content-Length", "27");
+    xhr.setRequestHeader("Connection", "keep-alive");
+    xhr.setRequestHeader("cache-control", "no-cache");
+
+    xhr.send(data);
+}
