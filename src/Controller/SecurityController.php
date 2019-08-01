0354a8ea0472b148ffcb804d38055a27096229d9
Alexander Anchishkin <alexanderanchishkin@gmail.com>
2019-08-01 03:18:18 +0300

    Fix redirect login to account (previous - recovery to account)

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -23,7 +23,7 @@ class SecurityController extends AbstractController
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
         if ($this->isGranted('ROLE_USER')) {
-            return $this->redirect('/');
+            return $this->redirect('/account');
         }
 
         // get the login error if there is one
