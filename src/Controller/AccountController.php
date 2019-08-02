9009953d1215a36ed9be6eb71e9f3c8c73b1907c
Alexander Anchishkin <alexanderanchishkin@gmail.com>
2019-08-02 04:21:04 +0300

    Remove debug code.

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -11,7 +11,6 @@ use App\Repository\UserRepository;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
@@ -152,8 +151,7 @@ class AccountController extends AbstractController
 
         $this->updateResults($this->getUser());
         $result_path = $this->getResultPath($this->getUser());
-                
-        return new JsonResponse($repository->getDonatorRewards($this->getUser()));
+                        
         return $this->render(
             'account/referrals.twig',
             [                
