2398e57f42ba30488df1d4f74f22cf0eb7e964b0
XanderBass <xander_bass@mail.ru>
2019-09-24 22:05:08 +0400

    Исправления модуля документов

--- src/Controller/DocumentController.php
+++ src/Controller/DocumentController.php
@@ -43,10 +43,7 @@ class DocumentController extends AbstractController
             if (!empty($_FILES['add_document_types']['tmp_name'])) {
                 $ff = $_FILES['add_document_types'];
                 $dd = rtrim($this->getParameter('documents_directory'), '/').'/';
-                $fn = $dd.uniqid().'-'.(\transliterator_transliterate(
-                    'Any-Latin; Latin-ASCII; [^A-Za-z0-9_] remove; Lower()',
-                    basename($ff['name']['file'])
-                ));
+                $fn = $dd.uniqid().'-'.($this->translit(basename($ff['name']['file'])));
                 if (move_uploaded_file($ff['tmp_name']['file'], $fn)) {
                     $document->setFile($fn);
                     $EM = $this->getDoctrine()->getManager();
@@ -137,5 +134,11 @@ class DocumentController extends AbstractController
         $entityManager->flush();
 
         return $this->redirect('/panel/documents');
+    }
+
+    public function translit($str) {
+        $rus = array('А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ъ', 'Ы', 'Ь', 'Э', 'Ю', 'Я', 'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я');
+        $lat = array('A', 'B', 'V', 'G', 'D', 'E', 'E', 'Gh', 'Z', 'I', 'Y', 'K', 'L', 'M', 'N', 'O', 'P', 'R', 'S', 'T', 'U', 'F', 'H', 'C', 'Ch', 'Sh', 'Sch', 'Y', 'Y', 'Y', 'E', 'Yu', 'Ya', 'a', 'b', 'v', 'g', 'd', 'e', 'e', 'gh', 'z', 'i', 'y', 'k', 'l', 'm', 'n', 'o', 'p', 'r', 's', 't', 'u', 'f', 'h', 'c', 'ch', 'sh', 'sch', 'y', 'y', 'y', 'e', 'yu', 'ya');
+        return str_replace($rus, $lat, $str);
     }
 }
