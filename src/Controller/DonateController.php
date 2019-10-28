50da2735d15ba3c6fdeb644ee87f1b4088c4af2d
notcgi <2ravno2@gmail.com>
2019-10-28 17:09:46 +0300

    .

--- src/Controller/DonateController.php
+++ src/Controller/DonateController.php
@@ -207,7 +207,7 @@ class DonateController extends AbstractController
         // if (!($req -> isRecurent())) {
         //     $dispatcher->dispatch(new PaymentFailure($req), PaymentFailure::NAME);}
         // else{
-            $dispatcher->dispatch(new RecurringPaymentFailure($req), RecurringPaymentFailure::NAME);}
+            $dispatcher->dispatch(new RecurringPaymentFailure($req), RecurringPaymentFailure::NAME);//}
         file_put_contents(dirname(__DIR__)."/../var/logs/failreq.log", date("d.m.Y H:i:s")."rec?".($req -> isRecurent()).'fail \n', FILE_APPEND);
 
         // Убрать напоминание о завершении платежа
@@ -389,7 +389,7 @@ class DonateController extends AbstractController
                     // if ($req -> isRecurent()) {
                     //     $dispatcher->dispatch(new RecurringPaymentFailure($req), RecurringPaymentFailure::NAME);}
                     // else{
-                        $dispatcher->dispatch(new PaymentFailure($req), PaymentFailure::NAME);}
+                        $dispatcher->dispatch(new PaymentFailure($req), PaymentFailure::NAME);//}
                     file_put_contents(dirname(__DIR__)."/../var/logs/failreq.log", date("d.m.Y H:i:s")."rec?".($req -> isRecurent()).'status \n', FILE_APPEND);
             }
 
